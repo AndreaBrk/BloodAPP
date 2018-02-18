86c9fab097bbc4c1ab11279948054e24b854f5b1
Andrea Borek <borekandrea@gmail.com>
2018-02-17 22:40:34 -0300

    mas

--- source/components/Dashboard/index.js
+++ source/components/Dashboard/index.js
@@ -388,7 +388,7 @@ class Dashboard extends React.Component {
 
               })
             }
-            { this.state.lat && this.state.lng
+            { this.state.lat && this.state.lng &&
               <Marker
                 name={'Posicion actual'}
                 position={{lat: this.state.lat, lng: this.state.lng}} />
