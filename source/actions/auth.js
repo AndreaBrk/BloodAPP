c43522b64078da0c3e46539b2a183ee74bfa2320
Andrea Borek <borekandrea@gmail.com>
2018-02-18 23:42:30 -0300

    cofiguration

--- source/actions/auth.js
+++ source/actions/auth.js
@@ -73,7 +73,7 @@ export function loginUser (creds) {
 
 export function resetPassword(params: Object) {
   return dispatch => {
-    let rUrl = `${generateApiUrl()}/user/reset_password`;
+    let rUrl = `${generateApiUrl()}/users/reset_password`;
     return apiRequest('POST', 'user', {
       url: rUrl,
       body: params,
