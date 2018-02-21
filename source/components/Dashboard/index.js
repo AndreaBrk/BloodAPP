b53cebf92848ec7f40310632ca7a1a663ada92bf
Andrea Borek <borekandrea@gmail.com>
2018-02-21 08:47:53 -0300

    added style

--- source/components/Dashboard/index.js
+++ source/components/Dashboard/index.js
@@ -177,7 +177,7 @@ class Dashboard extends React.Component {
 
       this.props.createDonationEvent(auth.headers(), creds)
       .then((data) => {
-        this.props.fetchData(auth.headers(), {vposLat, vposLng})
+        this.props.fetchData(auth.headers(), {postLat: vposLat, posLng: vposLng})
       })
     }
   }
